6ac595862224fa830dbadf860c38443b8f57c646
telepnef <telepnef@gmailcom>
2025-11-20 16:09:28 +0300

    5.4 Введение в Playwright. Практика с Playwright

---
+++ playwright_registration.py
@@ -0,0 +1,23 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    registration_email = page.get_by_test_id('registration-form-email-input').locator('input')
+    registration_email.fill("user.name@gmail.com")
+
+    registration_username = page.get_by_test_id('registration-form-username-input').locator('input')
+    registration_username.fill("username")
+
+    registration_password = page.get_by_test_id('registration-form-password-input').locator('input')
+    registration_password.fill("password")
+
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    expect(registration_button).to_be_visible()
+    registration_button.click()
+
+    dashboard_toolbar = page.get_by_test_id('dashboard-toolbar-title-text')
+    expect(dashboard_toolbar).to_be_visible()
+    expect(dashboard_toolbar).to_have_text("Dashboard")
---
+++ pw_auth.py
@@ -0,0 +1,22 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto(" https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+
+    email_field = page.locator("//div[@data-testid='login-form-email-input']//input")
+    email_field.fill("mail@mail.ru")
+
+    password_field = page.locator("//div[@data-testid='login-form-password-input']//input")
+    password_field.fill("password")
+
+    login_button = page.locator("// button[ @ id = 'login-page-login-button']")
+    login_button.click()
+
+    wrong_email_or_password_alert = page.locator("//div[@data-testid='login-page-wrong-email-or-password-alert']")
+    expect(wrong_email_or_password_alert).to_be_visible()
+    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
+
+
+    page.wait_for_timeout(2000)
